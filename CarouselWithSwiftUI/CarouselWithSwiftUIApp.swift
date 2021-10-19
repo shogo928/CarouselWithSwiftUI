6ce94848d22aef5d5ebb2f53de15a85334ff1ad8
shogo928 <s.shogo.dev@gmail.com>
2021-10-20 02:18:19 +0900

    first commit

---
+++ CarouselWithSwiftUI/CarouselWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselWithSwiftUIApp.swift
+//  CarouselWithSwiftUI
+//
+//  Created by sakumashogo on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CarouselWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
